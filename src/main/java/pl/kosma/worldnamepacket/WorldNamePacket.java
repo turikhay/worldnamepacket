6d0e0a33442341dcc5a0a3135c2a354b35e887c5
turikhay <git@turikhay.com>
2022-09-06 00:51:47 +0300

    Xaero's mods only read first integer from the response and use it as a world ID

--- src/main/java/pl/kosma/worldnamepacket/WorldNamePacket.java
+++ src/main/java/pl/kosma/worldnamepacket/WorldNamePacket.java
@@ -12,33 +12,38 @@ public class WorldNamePacket {
     public static final String CHANNEL_NAME_XAEROMAP = "xaeroworldmap:main";
     private static final String HEX_DIGITS = "0123456789abcdef";
 
-    public static byte[] formatResponsePacket(byte[] requestBytes, String worldName)
-    {
-        /**
-         * Workaround for a bug in VoxelMap for Fabric.
-         *
-         * The correct request-response format for well-behaved mods is:
-         * Request: 00 42 00
-         * Response: 00 42 <length> <world name>
-         *
-         * VoxelMap for Fabric didn't get the memo, it managed to botch both the request
-         * and the response. The VoxelMap format is:
-         * Request: 00 00 00 42
-         * Response: 42 <length> <world name>
-         *
-         * Hell is other people's code.
-         */
-        boolean useForgeDiscriminator = !Arrays.equals(requestBytes, VOXELMAP_FABRIC_REQUEST_PACKET);
-
-        byte[] worldNameBytes = worldName.getBytes(StandardCharsets.UTF_8);
-
+    public static byte[] formatResponsePacket(byte[] requestBytes, String worldName) {
         ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
-        if (useForgeDiscriminator)
-            responseStream.write(FORGE_PACKET_DISCRIMINATOR);
-        responseStream.write(VOXELMAP_MAGIC_NUMBER);
-        responseStream.write(worldNameBytes.length);
-        responseStream.write(worldNameBytes, 0, worldNameBytes.length);
-
+        if (requestBytes == null) {
+            int hash = worldName.hashCode();
+            responseStream.write(0); // LevelMapProperties marker
+            responseStream.write((hash & 0xFF000000) >> 24); // id (integer)
+            responseStream.write((hash & 0x00FF0000) >> 16); // ^
+            responseStream.write((hash & 0x0000FF00) >> 8);  // ^
+            responseStream.write((hash & 0x000000FF));          // ^
+        } else {
+            /**
+             * Workaround for a bug in VoxelMap for Fabric.
+             *
+             * The correct request-response format for well-behaved mods is:
+             * Request: 00 42 00
+             * Response: 00 42 <length> <world name>
+             *
+             * VoxelMap for Fabric didn't get the memo, it managed to botch both the request
+             * and the response. The VoxelMap format is:
+             * Request: 00 00 00 42
+             * Response: 42 <length> <world name>
+             *
+             * Hell is other people's code.
+             */
+            boolean useForgeDiscriminator = !Arrays.equals(requestBytes, VOXELMAP_FABRIC_REQUEST_PACKET);
+            byte[] worldNameBytes = worldName.getBytes(StandardCharsets.UTF_8);
+            if (useForgeDiscriminator)
+                responseStream.write(FORGE_PACKET_DISCRIMINATOR);
+            responseStream.write(VOXELMAP_MAGIC_NUMBER);
+            responseStream.write(worldNameBytes.length);
+            responseStream.write(worldNameBytes, 0, worldNameBytes.length);
+        }
         return responseStream.toByteArray();
     }
 
